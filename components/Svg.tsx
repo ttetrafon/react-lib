e1880677f49e93b0e5124c2f53a243baf4876293
ttetrafon <ttetrafon@yahoo.gr>
2026-05-28 15:18:54 +0300

    Svg component: hover-fill

--- components/Svg.tsx
+++ components/Svg.tsx
@@ -5,17 +5,18 @@ export const svgNames = Object.keys(svgs) as SvgName[];
 export interface SvgProps {
   name: SvgName;
   fill?: string;
+  hoverFill?: string | undefined;
   className?: string;
 }
 
-export function Svg({ name, fill = "#000000", className }: SvgProps) {
+export function Svg({ name, fill = "#000000", hoverFill, className }: SvgProps) {
   return (
     <svg
       className={`w-full h-full ${className || ""}`}
       viewBox="0 -960 960 960"
       width="100%"
       height="100%"
-      fill={fill}
+      fill={hoverFill || fill}
       xmlns="http://www.w3.org/2000/svg"
     >
       <path d={svgs[name]} />
